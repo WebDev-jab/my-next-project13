c33265137d8ce8908f8990288449b140647c1ac9
WebDev-jab <WebDev@jab.or.jp>
2025-04-21 16:26:21 +0900

    コメント

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>タイトルテキスト</h1>
-      <p className={styles.description}>texttexttext</p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>タイトルテキスト</h1>
+        <p className={styles.description}>texttexttext</p>
+      </div>
+      <img className={styles.bgimg} src="/img-mv.jpg" alt=""></img>
+    </section>
   );
 }
